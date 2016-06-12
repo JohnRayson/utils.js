6b49dedfed09eb057fb98e75b73fa75730ab55a1
John Rayson <j.d.rayson@googlemail.com>
2016-06-12 18:38:05 +0100

    Added loads of stuff. Of most significance: * utils.await() allows a number of ajax calls to be made then a callback executed * combined the alert() / userChoice() functions, and completed userChoice()

--- utils.js
+++ utils.js
@@ -2,6 +2,15 @@
 // duplicate functions / variables might therefore behave in an unexpected manner depending on load order
 var utils = utils || {};
 
+// add the no-close style (this hides the close icon on dialogs - jQuery UI recommended way of doing it. http://api.jqueryui.com/dialog/
+var head = document.getElementsByTagName('head')[0];
+var styles = document.createElement("style");
+styles.innerHTML = ".no-close .ui-dialog-titlebar-close { display: none; } "
+                 + ".utils-centre { text-align: center !important; margin-left: auto !important; margin-right: auto !important; }"
+                 + ".utils-plain-list { list-style-type:none; padding: 0px; }"
+; // on this line so its easy to add new ones without accidentally screwing it up
+head.appendChild(styles);
+
 utils.require = {
     jQuery: { type: "script", dependent: null, name: "jQuery", url: "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js", check: function () { return window.jQuery; }, state: "pending" },
     jQueryUI: { type: "script", dependent: "jQuery", name: "jQueryUI", url: "https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js", check: function () { return window.jQuery && window.jQuery.ui; }, state: "pending" },
@@ -83,6 +92,7 @@ utils.get = function (key, info)
     // not found
     return false;
 }
+// store things locally
 utils.localStorage = function (appId, persist)
 {
     var _storage = (persist ? localStorage : sessionStorage);
@@ -183,39 +193,23 @@ utils.initilize = function ()
     //basic alert dialogue
     utils.alert = function (message)
     {
-        var id = utils.createUUID();
-        var html = "<div id='" + id + "' title='alert'>"
-                 + "<h1>" + message + "</h1>"
-                 + "</div>";
-        $(html).dialog(
-        {
-            modal: true,
-            close: function ()
-            {
-                $(this).empty().remove();
-            },
-            buttons:
-            {
-                "OK": function ()
-                {
-                    $(this).dialog("close");
-                }
-            }
-        });
+        utils.userChoice("Alert", message);
     }
-    // present a series of options to the user - not finished - alert() should call through to this as well, and there should be a short hand confirm() callign this
-    utils.userChoice = function (message, buttons)
+    // present a series of options to the user
+    utils.userChoice = function (title, message, options)
     {
+        var viewportWidth = $(window).width();
+        var viewportHeight = $(window).height();
+
         var id = utils.createUUID();
-        var html = "<div id='" + id + "' title='alert'>"
-                 + "<h1>" + message + "</h1>"
+        var html = "<div id='" + id + "' title='" + title + "'>" // overflow: hidden - control the scrolling in what ever you pass in
+                 + "<div style='overflow: hidden; max-height: " + viewportHeight / 2 + "px; max-width: " + viewportWidth / 2 + "px;'>" + message + "</div>"
                  + "</div>";
 
-        $(html).dialog(
-        {
-            modal: true,
-            buttons: buttons
-        });
+        var defaults = { dialogClass: "no-close", modal: true, closeOnEscape: false, autoOpen: true, buttons: { "Close": function () { $(this).empty().remove(); }}};
+        var settings = $.extend({},defaults, options);
+
+        $(html).dialog(settings);
     };
 
     // some styling for inputs
@@ -248,7 +242,7 @@ utils.initilize = function ()
     })(jQuery);
 
     // wrapper to make Ajax calls easy - being as most of the time the settigns will be the same
-    utils.ajaxOptions = { baseURL: "", type: "GET", contentType: "application/json", data: "", path: "", beforeSend: function (xhr) { }, success: function (data) { }, error: function (data) { } };
+    utils.ajaxOptions = { global: true, baseURL: "/", type: "GET", contentType: "application/json", data: "", path: "", beforeSend: function (xhr) { }, success: function (data) { }, error: function (data) { } };
     utils.setAjaxOptions = function (options)
     {
         //  just merge what we pass in with whats already there
@@ -260,14 +254,59 @@ utils.initilize = function ()
 
         $.ajax(
         {
+            global: settings.global,
             url: settings.baseURL + settings.path,
             type: settings.type,
             contentType: settings.contentType,
-            data: settings.data,
+            data: JSON.stringify(settings.data), // otherwise jQuery sends it as Form encoded
             beforeSend: settings.beforeSend,
-            success: function (data) { settings.success(data) },
-            error: function (data) { settings.error(data) }
+            success: function (data) { utils.parseAjaxReply(data, settings.success); },
+            error: function (data) { utils.parseAjaxReply(data, settings.error); }
         });
+    }
+    utils.parseAjaxReply = function (data, replyFunc)
+    {
+        // to remove the wrapping
+        if (data["d"])
+            data = data["d"];
+        replyFunc(data);
+    }
+    // wait for a series (Array) of ajax function calls to return, then execute a function
+    utils.await = function (calls, complete)
+    {
+        var set = {
+            count: calls.length,
+            replied: 0,
+            check: function()
+            {
+                this.replied++;
+                if(this.replied == this.count)
+                    complete();
+            }
+        }
+
+        for (var call = 0; call<calls.length; call++)
+        {
+            (function(that)
+            {
+                that.baseSuccess = that.success;
+                that.baseError = that.error;
+                that.success = function (data)
+                {
+                    if(that.baseSuccess)
+                        that.baseSuccess(data);
+                    set.check();
+                };
+                that.error = function(data)
+                {
+                    if(that.baseError)
+                        that.baseError(data);
+                    set.check();
+                };
+                // send off the call
+                utils.ajax(that);
+            })(calls[call]);
+        }
     }
 
     if (utils.initilizeComplete)
